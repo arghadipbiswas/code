279c6b3299e24e25993aaa6e0d27be78b2bf9b3e
Arghadip <arghadipbiswas4076@gmail.com>
2022-10-25 00:15:29 +0530

    added new 3 files

---
+++ month_budget.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int d=30*b;
+	    if(d<=a)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
